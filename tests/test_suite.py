930c52695ce9869f209eb8defa760a0fcb3a2066
MarkDownThere <mrcgiulini@gmail.com>
2022-03-24 11:27:09 +0100

    added get_clust and corresponding test

--- tests/test_suite.py
+++ tests/test_suite.py
@@ -7,10 +7,34 @@ import os
 import numpy as np
 import pandas as pd
 # import utils modules
-from utils import check_volume
+from utils import check_volume, get_clust
 
 def test_volume():
+    """test correct calculation of the volume in a trivial case"""
     np_arr = np.array([[0,1],[0,0],[1,1]])
     df = pd.DataFrame(np_arr)
     print(df)
     assert check_volume(df,df.shape[1]) == 2
+
+def test_at_clust():
+    """test correct at clustering in a trivial case"""
+    d = {'a': [1, 0, 1, 1, 1], 'b': [-1, 4, -1, 4, 0]}
+    full_df = pd.DataFrame(d)
+    mapping = np.array([0,1])
+    at_df = get_clust(full_df,mapping)
+    expected_data = {'a': [0,1,1,1], 'b': [4,-1,0,4], 'records': [1,2,1,1]}
+    expected_at_df = pd.DataFrame(expected_data)
+    assert at_df.equals(expected_at_df)
+
+def test_cg_clust():
+    """test correct cg clustering in a trivial case"""
+    at_d = {'a': [0,1,1,1], 'b': [4,-1,0,4]}
+    at_df = pd.DataFrame(at_d)
+    mapping = np.array([0])
+    cg_df = get_clust(at_df,mapping)
+    expected_data = {'a': [0,1], 'records': [1,3]}
+    expected_cg_df = pd.DataFrame(expected_data)
+    assert cg_df.equals(expected_cg_df)
+
+#def test_cg_clust_error():
+    """cg clustering should raise an error if equal rows are present"""
--- utils.py
+++ utils.py
@@ -15,3 +15,39 @@ def check_volume(dataframe,ncols):
         V = V[0][0]
     print("V = ", V)
     return V
+
+def get_clust(dataframe,mapping):
+    """
+    compute the clustering
+    """
+    clust = dataframe.groupby(dataframe.columns[mapping].tolist()).size().reset_index().rename(columns={0:'records'})
+    clust.columns = pd.concat([pd.Series(dataframe.columns[mapping]), pd.Series("records")])
+    return clust
+
+def calculate_entropies(cg_clust):
+    """
+    calculate resolution and relevance from the coarse-grained clustering
+    """
+    hs = entropy(cg_clust["records"])
+    ks = np.unique(cg_clust["records"], return_counts=True)
+    pk = np.multiply(ks[0], ks[1])
+    hk = entropy(pk)
+    return hs,hk
+
+def calculate_smap(at_clust, cg_clust, V):
+    """
+    calculate the mapping entropy in its two definitions
+    """
+    omega_1 = at_clust.groupby(at_clust.columns[mapping].tolist()).size().reset_index().rename(columns={0:'records'})
+    # smeared probability distribution
+    p_bar_r = cg_clust["records"]/df.shape[0]/omega_1["records"]
+    new_p_bar = pd.concat([omega_1,p_bar_r],axis=1) # to keep track of all the cg configurations
+    # state-wise mapping entropy
+    mapping_entropy = []
+    for n in range(at_clust.shape[0]):
+        s = np.where((at_clust.iloc[n,mapping] == new_p_bar.iloc[:,:-2]).all(1) == True)[0][0]
+        mapping_entropy.append(pr[n]*np.log(pr[n]/new_p_bar.iloc[s,-1]))
+    tot_smap = sum(mapping_entropy)
+    # infinite sampling mapping entropy
+    delta_s_conf = (len(at_clust.columns) - 1 - len(mapping))*np.log(V) - entropy(at_clust["records"]) + hs
+    return smap, delta_s_conf
